abab1bd43df30abf59dc1aef7f8ec23738f1497b
sandeep194920 <sandeepamaranath@gmail.com>
2020-02-01 11:33:25 -0500

    added Open Sans fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div>Sandeep</div>;
 }
 
 export default App;
