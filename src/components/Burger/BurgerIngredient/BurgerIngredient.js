8080d7b4d7f4ce55fa0d178ef724cfcf01c0503b
sandeep194920 <sandeepamaranath@gmail.com>
2020-02-01 15:16:39 -0500

    turned burgerIngredient into classbased simply and applied prop-types

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./BurgerIngredient.module.css";
-const burgerIngredient = (props) => {
-  return (
-    <div>
-      <div className={classes.Salad}></div>
-      {/* Here we need to get props what we need ingredients and then render accordingly. So we need to modify this now */}
-    </div>
-  );
-};
-export default burgerIngredient;
+import PropTypes from "prop-types";
+
+class BurgerIngredient extends Component {
+  render() {
+    let ingredient = null;
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingredient = <div className={classes.BreadBottom}></div>;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>
+        );
+        break;
+      case "salad":
+        ingredient = <div className={classes.Salad}></div>;
+        break;
+      case "meat":
+        ingredient = <div className={classes.Meat}></div>;
+        break;
+      case "cheese":
+        ingredient = <div className={classes.Cheese}></div>;
+        break;
+      case "bacon":
+        ingredient = <div className={classes.Bacon}></div>;
+        break;
+    }
+    return ingredient;
+  }
+}
+
+// Implementing prop-types for this component. This could be a functional component, but simply turned into class based
+
+BurgerIngredient.PropTypes = {
+    type = PropTypes.string.isRequired // It says, the prop type should be string and is required
+}
+
+export default BurgerIngredient;
