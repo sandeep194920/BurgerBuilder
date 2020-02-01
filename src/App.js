b40d8704a69b24a4728d157af93f983ef4adbb20
sandeep194920 <sandeepamaranath@gmail.com>
2020-02-01 12:02:32 -0500

    now the basic folder structure of Layout is done. We now need to start with BurgerBuilder component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Layout from "./components/Layout/Layout";
 function App() {
   return (
     <div>
-      <Layout />
+      <Layout>
+        <div>BurgerBuilder</div>{" "}
+        {/*  In above div we need to replace with real BurgerBuilder component*/}
+      </Layout>
     </div>
   );
 }
