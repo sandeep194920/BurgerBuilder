4748e2d909ffbad33b8cedce486294f781a480bf
sandeep194920 <sandeepamaranath@gmail.com>
2020-02-13 20:05:57 -0500

    added SidDrawer but the logo looks big. So logo and navigation items need to be taken care of

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import Sidedrawer from "../Navigation/SideDrawer/SideDrawer";
 const layout = (props) => (
   <Aux>
     {/* <div>Toolbar, Sidedrawer,Backdrop</div> */}
     <Toolbar />
+    <Sidedrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
