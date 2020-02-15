5ce9a5f1947e52d20a6fa362b5c60fed0c27d675
sandeep194920 <sandeepamaranath@gmail.com>
2020-02-14 23:31:02 -0500

    added toggle button and functionality to toggle sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import Sidedrawer from "../Navigation/SideDrawer/SideDrawer";
-const layout = (props) => (
-  <Aux>
-    {/* <div>Toolbar, Sidedrawer,Backdrop</div> */}
-    <Toolbar />
-    <Sidedrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  // The click listener of the backdrop component of SidDrawer is handled here because this is the common component
+  // that has both Toolbar and SidDrawer. This connection is important because we have a menu button in the toolbar
+  // which when clicked opens the sidedrawer. Hence we turn this component from function to class so that we can set state
 
-export default layout;
+  // state has been primarily added for the above mentioned reasons. This component initially was a functional based
+  state = {
+    showSideDrawer: false
+  };
+
+  sidDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sidDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true });
+  };
+
+  sidDrawerToggleHandler = () => {
+    this.setState((prevState) => ({
+      showSideDrawer: !prevState.showSideDrawer
+    }));
+  };
+
+  render() {
+    return (
+      <Aux>
+        {/* <div>Toolbar, Sidedrawer,Backdrop</div> */}
+        <Toolbar drawerToggleClicked={this.sidDrawerToggleHandler} />
+        {/* Instead of sideDrawerToggleHandler above in the toolbar, we could have used sidDrawerOpenHandler because
+        we are using this button just to open the sideDrawer. However, it might help us in future just in case */}
+        <Sidedrawer
+          closed={this.sidDrawerClosedHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,12 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    {/* <div onClick={props.clickedMenu}>MENU</div> */}
+    {/* The above component is now outsourced into its own, so it becomes below code */}
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <div className={classes.Logo}>
       <Logo />
     </div>
