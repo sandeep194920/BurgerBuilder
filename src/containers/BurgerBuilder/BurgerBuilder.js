0c7eaa5e8b5bcfad0a68d8f0c1c5af0bf96c52aa
sandeep194920 <sandeepamaranath@gmail.com>
2020-02-15 22:53:33 -0500

    implemented hoc withErrorHandler which handles the axios errors for any component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 // Creating global constant for Ingredient prices
 const INGREDIENT_PRICES = {
@@ -166,6 +167,7 @@ class BurgerBuilder extends Component {
         console.log(response);
       })
       .catch((error) => {
+        console.log(error);
         this.setState({ loading: false, purchasing: false }); // purchasing is the prop which shows modal, so we set it to false once the purchasing is done to close the modal.
       });
   };
@@ -218,4 +220,4 @@ class BurgerBuilder extends Component {
     );
   }
 }
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Aux from "../Aux/Aux";
+import Modal from "../../components/UI/Modal/Modal";
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    // This state is required only for the error which is used to show the modal below
+    state = {
+      error: null
+    };
+    // Note that this method will be executed after the Wrapped component (BurgerBuilder in this case)is executed.
+    // That means the axios instance written in BurgerBuilder is executed and then reaches the interceptors written
+    // inside this below componentDidMount()
+    componentDidMount() {
+      //used to clear error if any occured in response interceptors below
+      axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req; // We need to return request in the interceptor request so that it proceeds
+      });
+      //The below method is the method we are interested in but we are writing the interceptors.requests just to
+      // clear the errors if any occured during the response interceptors below
+      axios.interceptors.response.use(
+        (res) => res, // We need to return response in the interceptor response so that it proceeds
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    // We use this errrorConfirmedHandler to clear any errors (setting back to null) once the backdrop is clicked which is in the modal
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      return (
+        <Aux>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
