0458c005531879d41ac0501fae286dcfea882fce
sandeep194920 <sandeepamaranath@gmail.com>
2020-04-01 23:59:37 -0400

    made case more leaner in reducers

--- src/store/reducer/burgerBuilder.js
+++ src/store/reducer/burgerBuilder.js
@@ -13,51 +13,64 @@ const initialState = {
   totalPrice: 4,
   error: false
 };
+
+// utility function to reduce number of lines in ADD_INGREDIENT case
+const addIngredient = (state, action) => {
+  const updatedIngredient = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+  };
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  const updatedState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+  };
+
+  return updateObject(state, updatedState);
+};
+
+// utility function to reduce number of lines in REMOVE_INGREDIENT case
+const removeIngredient = (state, action) => {
+  const updatedIng = {
+    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+  };
+  const updatedIngs = updateObject(state.ingredients, updatedIng);
+  const updatedSt = {
+    ingredients: updatedIngs,
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+  };
+
+  return updateObject(state, updatedSt);
+};
+
+// utility function to reduce number of lines in SET_INGREDIENT case
+const setIngredients = (state, action) => {
+  return updateObject(state, {
+    ingredients: {
+      salad: action.ingredients.salad,
+      bacon: action.ingredients.bacon,
+      meat: action.ingredients.meat,
+      cheese: action.ingredients.cheese
+    },
+    error: false, // this is important bcoz if we had error before then this clears it out
+    totalPrice: 4 // this helps when redirecting to homepage after redirection after first order is been placed
+  });
+};
+
+// utility function to reduce number of lines in FETCH_INGREDIENTS_FAILED case
+const fetchIngredientsFailed = (state, action) => {
+  return updateObject(state, { error: true });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      const updatedIngredient = {
-        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-      };
-      const updatedIngredients = updateObject(
-        state.ingredients,
-        updatedIngredient
-      );
-      const updatedState = {
-        ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-      };
-
-      return updateObject(state, updatedState);
-
+      return addIngredient(state, action);
     case actionTypes.REMOVE_INGREDIENT:
-      const updatedIng = {
-        [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-      };
-      const updatedIngs = updateObject(state.ingredients, updatedIng);
-      const updatedSt = {
-        ingredients: updatedIngs,
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-      };
-
-      return updateObject(state, updatedSt);
-
+      return removeIngredient(state, action);
     case actionTypes.SET_INGREDIENTS:
-      return updateObject(state, {
-        ingredients: {
-          salad: action.ingredients.salad,
-          bacon: action.ingredients.bacon,
-          meat: action.ingredients.meat,
-          cheese: action.ingredients.cheese
-        },
-        error: false, // this is important bcoz if we had error before then this clears it out
-        totalPrice: 4 // this helps when redirecting to homepage after redirection after first order is been placed
-      });
-
+      return setIngredients(state, action);
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state
-      };
+      return fetchIngredientsFailed(state, action);
     default:
       return state;
   }
--- src/store/reducer/order.js
+++ src/store/reducer/order.js
@@ -7,41 +7,63 @@ const initialState = {
   purchased: false
 };
 
+// utility function to reduce number of lines in PURCHASE_INIT case
+const purchaseInit = (state, action) => {
+  return updateObject(state, { purchased: false });
+};
+// utility function to reduce number of lines in PURCHASE_BURGER_START case
+const purchaseBurgerStart = (state, action) => {
+  return updateObject(state, { loading: false });
+};
+// utility function to reduce number of lines in PURCHASE_BURGER_SUCCESS case
+const purchaseBurgerSuccess = (state, action) => {
+  const newOrder = updateObject(action.orderData, {
+    id: action.orderID
+  });
+
+  return updateObject(state, {
+    loading: false,
+    purchased: true,
+    orders: state.orders.concat({ newOrder })
+  });
+};
+// utility function to reduce number of lines in PURCHASE_BURGER_FAIL case
+const purchaseBurgerFail = (state, action) => {
+  return updateObject(state, { loading: false });
+};
+// utility function to reduce number of lines in FETCH_ORDERS_START case
+const fetchOrdersStart = (state, action) => {
+  return updateObject(state, { loading: true });
+};
+// utility function to reduce number of lines in FETCH_ORDERS_SUCCESS case
+const fetchOrdersSuccess = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    orders: action.orders
+  });
+};
+// utility function to reduce number of lines in FETCH_ORDERS_FAIL case
+const fetchOrdersFail = (state, action) => {
+  return updateObject(state, { loading: false });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
-      return updateObject(state, { purchased: false });
-
+      return purchaseInit(state, action);
     case actionTypes.PURCHASE_BURGER_START:
-      return updateObject(state, { loading: false });
-
+      return purchaseBurgerStart(state, action);
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = updateObject(action.orderData, {
-        id: action.orderID
-      });
-
-      return updateObject(state, {
-        loading: false,
-        purchased: true,
-        orders: state.orders.concat({ newOrder })
-      });
-
+      return purchaseBurgerSuccess(state, action);
     case actionTypes.PURCHASE_BURGER_FAIL:
-      return updateObject(state, { loading: false });
-
+      return purchaseBurgerFail(state, action);
     // cases for getting orders from backend
     case actionTypes.FETCH_ORDERS_START:
-      return updateObject(state, { loading: true });
-
+      return fetchOrdersStart(state, action);
     case actionTypes.FETCH_ORDERS_SUCCESS:
-      return updateObject(state, {
-        loading: false,
-        orders: action.orders
-      });
-
+      return fetchOrdersSuccess(state, action);
     case actionTypes.FETCH_ORDERS_FAIL:
-      return updateObject(state, { loading: false });
-
+      return fetchOrdersFail(state, action);
     default:
       return state;
   }
