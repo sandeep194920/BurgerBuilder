ee921ac8a0616e8c9d59c7e5c884bb927fdefb67
sandeep194920 <sandeepamaranath@gmail.com>
2020-04-04 22:41:40 -0400

    user signup process

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,9 +1,11 @@
 import * as actionTypes from "./actionTypes";
+// Not using axios-orders instance but using default axios
+import axios from "axios";
 
 // sync action : Essentially used to show the auth action has started hence the loading can be set to true in store to show spinner
 export const authStart = () => {
   return {
-    type: actionTypes.AUTH_START
+    type: actionTypes.AUTH_START,
   };
 };
 
@@ -11,7 +13,7 @@ export const authStart = () => {
 export const authSuccess = (authData) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    authData: authData,
   };
 };
 
@@ -19,14 +21,32 @@ export const authSuccess = (authData) => {
 export const authFail = (error) => {
   return {
     type: actionTypes.AUTH_FAIL,
-    error: error
+    error: error,
   };
 };
 
 // aysnc action : to get from backend. This info got from async action below will be sent to reducer through sync actions above
 export const auth = (email, password) => {
   return (dispatch) => {
-    console.log("Reached auth");
     dispatch(authStart()); // All this dispactches can be seen in redux devtools in chrome browser to see if this works
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true, // You need to have this info as per the docs of firebase which we need to pass to firebase to get the token during signup. You can find it at https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvhN5t3lHIQ9f67EP5Jpu_Yu2dXTTIQww",
+        authData
+      )
+      .then((response) => {
+        // here in response.data which is dipatched below, we get the token. This is dispatched in authSuccess() below and stored in reducer
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
   };
 };
